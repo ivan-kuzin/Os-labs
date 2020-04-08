3e22d9be61d707a25f461d2fe989eb79bf4ce68c
ivan-kuzin <56124096+ivan-kuzin@users.noreply.github.com>
2020-04-08 16:51:24 +0300

    fix

--- 1/solution.sh
+++ 1/solution.sh
@@ -3,5 +3,5 @@ mkdir 8091 && cd 8091
 mkdir Kuzin && cd Kuzin
 date > Ivan.txt
 date --date "next Mon" > filedate.txt
-cat Kuzin.txt filedate.txt > result.txt
+cat Ivan.txt filedate.txt > result.txt
 cat result.txt
